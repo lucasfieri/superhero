a35b369f0c0536fd81e2f563a65159724812f51e
Lucas fieri <lucasfieri@gmail.com>
2021-01-21 19:17:58 -0300

    feat: create hometitle molecule

---
+++ src/components/molecules/HomeTitle/HomeTitle.jsx
@@ -0,0 +1,13 @@
+import Title from '../../atoms/Title/Title'
+import Subtitle from '../../atoms/Subtitle/Subtitle'
+
+const HomeTitle = () => {
+  return (
+    <>
+      <Title text='Superhero' />
+      <Subtitle text='Quem é a pessoa por trás das máscaras da justiça?' />
+    </>
+  )
+}
+
+export default HomeTitle
