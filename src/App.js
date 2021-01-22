57672e039f3ef2184ff60800378c9f86a78e550c
Lucas fieri <lucasfieri@gmail.com>
2021-01-22 16:48:00 -0300

    feat: calls glossary component at app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import SearchBox from './components/molecules/SearchBox/SearchBox'
 import HomeTitle from './components/molecules/HomeTitle/HomeTitle'
 import Container from './components/atoms/Container/Container'
+import Glossary from './components/atoms/Glossary/Glossary'
 
 function App () {
   return (
     <Container>
       <HomeTitle />
       <SearchBox />
+      <Glossary />
     </Container>
   )
 }
