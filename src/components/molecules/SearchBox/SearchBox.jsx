d70364b7c3138f9311fa6a0f1b7334bd9bd132d5
Lucas fieri <lucasfieri@gmail.com>
2021-01-22 16:47:37 -0300

    style: stylize searchbox molecule

--- src/components/molecules/SearchBox/SearchBox.jsx
+++ src/components/molecules/SearchBox/SearchBox.jsx
@@ -1,11 +1,13 @@
 import Button from '../../atoms/Button/Button'
 import Input from '../../atoms/Input/Input'
 import { ReactComponent as SearchIcon } from '../../../assets/images/icon_search.svg'
+import './style.scss'
+
 const SearchBox = () => {
   return (
     <div className='searchbox'>
-      <Button handleClick={() => console.log('aaa')} icon={<SearchIcon />} />
-      <Input name='search_input' />
+      <Input name='search_input' className='search_input' />
+      <Button handleClick={() => console.log('aaa')} icon={<SearchIcon />} className='search_button' />
     </div>
   )
 }
