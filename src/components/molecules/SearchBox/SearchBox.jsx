d01a9d32e00e4153ef2ed096650d187a2970ed0f
Lucas fieri <lucasfieri@gmail.com>
2021-01-24 16:28:35 -0300

    feat: add state on searchbox input

--- src/components/molecules/SearchBox/SearchBox.jsx
+++ src/components/molecules/SearchBox/SearchBox.jsx
@@ -1,13 +1,25 @@
+import { useState } from 'react'
 import Button from '../../atoms/Button/Button'
 import Input from '../../atoms/Input/Input'
 import { ReactComponent as SearchIcon } from '../../../assets/images/icon_search.svg'
 import './style.scss'
 
 const SearchBox = () => {
+  const [inputSearch, setInputSearch] = useState('')
+
   return (
-    <div className='searchbox'>
-      <Input name='search_input' className='search_input' />
-      <Button handleClick={() => console.log('aaa')} icon={<SearchIcon />} className='search_button' />
+    <div className='searchBox'>
+      <Input
+        name='search_input'
+        className='searchBox__input'
+        value={inputSearch}
+        onChange={(e) => setInputSearch(e.target.value)}
+      />
+      <Button
+        variation='btn-primary searchBox__button'
+        handleClick={() => console.log('aaa')}
+        icon={<SearchIcon />}
+      />
     </div>
   )
 }
