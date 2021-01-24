3dea5fd146aedcfb499e41e2c290e3fb39d3d530
Lucas fieri <lucasfieri@gmail.com>
2021-01-24 16:17:36 -0300

    fix: add null value classname prop on subtitle component

--- src/components/atoms/Subtitle/Subtitle.jsx
+++ src/components/atoms/Subtitle/Subtitle.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import './style.scss'
 
-const Subtitle = ({ text, className = '' }) => {
+const Subtitle = ({ text, className = null }) => {
   return (
     <h4 className={`subtitle ${className}`}>
       {text}
