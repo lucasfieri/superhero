02320a944438d5231ac9de3553382e34e056c119
Lucas fieri <lucasfieri@gmail.com>
2021-01-25 23:48:13 -0300

    feat: move searchbox molecule folder

--- src/components/organims/SearchBox/SearchBox.jsx
+++ src/components/organims/SearchBox/SearchBox.jsx
@@ -1,14 +1,18 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { bool } from 'prop-types'
 import Button from '../../atoms/Button/Button'
 import Input from '../../atoms/Input/Input'
 import { ReactComponent as SearchIcon } from '../../../assets/images/icon_search.svg'
+import { ReactComponent as FilterIcon } from '../../../assets/images/icon_filter.svg'
 import { fetchHeroes, changeInputSearch } from '../../../ducks/search'
+import Popover from '../../molecules/Popover/Popover'
+import FilterForm from '../FilterForm/FilterForm'
 
 import './style.scss'
 
-const SearchBox = () => {
+const SearchBox = ({ hasFilter }) => {
   const dispatch = useDispatch()
-  const { inputSearch } = useSelector(state => ({ inputSearch: state.inputSearch }))
+  const { inputSearch } = useSelector(state => ({ inputSearch: state.search.inputSearch }))
   return (
     <div className='searchBox'>
       <Input
@@ -22,8 +26,20 @@ const SearchBox = () => {
         handleClick={() => dispatch(fetchHeroes())}
         icon={<SearchIcon />}
       />
+      {hasFilter && (
+        <Popover
+          buttonIcon={<FilterIcon />}
+          buttonVariation='btn-secondary'
+          positions={['bottom']}
+          content={<FilterForm />}
+        />
+      )}
     </div>
   )
 }
 
 export default SearchBox
+
+SearchBox.propTypes = {
+  hasFilter: bool
+}
