100d7754ed69ec7823a45fcc84ec43a536bdcf54
Lucas fieri <lucasfieri@gmail.com>
2021-01-25 23:46:30 -0300

    refactor: refactor search duck

--- src/ducks/search.js
+++ src/ducks/search.js
@@ -1,9 +1,11 @@
 import Immutable from 'seamless-immutable'
+import { push } from 'connected-react-router'
+
 import SuperHeroAPI from '../services'
 
 export const Types = {
-  CHANGE: 'search/CHANGE',
-  CLICK: 'serach/CLICK',
+  CHANGE_SEARCH: 'search/CHANGE_SEARCH',
+  CLICK_SEARCH: 'search/CLICK_SEARCH',
   FETCH_REQUEST: 'search/FETCH_REQUEST',
   FETCH_SUCCESS: 'search/FETCH_SUCCESS',
   FETCH_FAIL: 'search/FETCH_FAIL'
@@ -11,57 +13,71 @@ export const Types = {
 
 const initialState = Immutable({
   inputSearch: '',
+  isFetching: false,
   results: [{}]
 })
 
 export default function reducer (state = initialState, action) {
   switch (action.type) {
-    case Types.CHANGE:
+    case Types.CHANGE_SEARCH:
       return {
         ...state,
         inputSearch: action.payload
       }
-    case Types.CLICK:
+    case Types.CLICK_SEARCH:
       return {
         ...state
       }
     case Types.FETCH_REQUEST:
       return {
-        ...state
+        ...state,
+        isFetching: true
       }
     case Types.FETCH_SUCCESS:
       return {
         ...state,
-        results: action.payload
+        results: action.payload,
+        isFetching: false
       }
     case Types.FETCH_FAIL:
       return {
         ...state,
-        results: [{}]
+        results: [{}],
+        isFetching: false
       }
     default:
       return state
   }
 }
 
-export const fetchHeroes = () => {
-  return async function (dispatch, getState) {
-    dispatch({ type: Types.CLICK })
-    dispatch({ type: Types.FETCH_REQUEST })
-    try {
-      const { search } = getState()
-      const response = await SuperHeroAPI.getByQuery(search.inputSearch)
-      console.log(response)
-      return dispatch({ type: Types.FETCH_SUCCESS, payload: response.data.results })
-    } catch (e) {
-      dispatch({ type: Types.FETCH_FAIL })
-    }
+export function changeInputSearch (inputSearch) {
+  return {
+    type: Types.CHANGE_SEARCH,
+    payload: inputSearch
   }
 }
 
-export function changeInputSearch (inputSearch) {
-  return {
-    type: Types.CHANGE,
-    payload: inputSearch
+export const fetchHeroes = () => {
+  return async function (dispatch, getState) {
+    dispatch({ type: Types.CLICK_SEARCH })
+    const { search } = getState()
+    dispatch(push(`/${search.inputSearch}`))
+  }
+}
+
+export const fetchHeroesOnLoadPage = (path) => {
+  return async function (dispatch) {
+    dispatch({ type: Types.FETCH_REQUEST })
+    dispatch({ type: Types.CHANGE_SEARCH, payload: path })
+    try {
+      const response = await SuperHeroAPI.getByQuery(path)
+      if (response.data.error) {
+        throw new Error()
+      }
+      dispatch({ type: Types.FETCH_SUCCESS, payload: response?.data?.results })
+    } catch (e) {
+      console.error('error', e)
+      dispatch({ type: Types.FETCH_FAIL })
+    }
   }
 }
